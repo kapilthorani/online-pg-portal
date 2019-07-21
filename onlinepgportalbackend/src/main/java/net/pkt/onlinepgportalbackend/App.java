e336232eb4c522aec8e52345b2db0bb024869df0
kapilthorani <thoranikt@gmail.com>
2019-07-21 18:57:07 +0530

    Project Setup completed successfully

---
+++ onlinepgportalbackend/src/main/java/net/pkt/onlinepgportalbackend/App.java
@@ -0,0 +1,13 @@
+package net.pkt.onlinepgportalbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
